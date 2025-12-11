b79a29167453291b02ed2e57ed206da40016754c
Rovvit <vitazzz96@gmail.com>
2025-12-11 19:19:05 +0200

    try-except

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import os
 import json
 import stripe
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, Request, status
+from fastapi import FastAPI, Request, status, HTTPException
 from starlette.responses import JSONResponse
 from tortoise import Tortoise
-
 from db.TelegramUser import get_telegram_user, create_telegram_user
-from db.models import Customer
 from tortoise_config import TORTOISE_ORM
 from utils.logger import logger
 from db.PaymentIntent import save_payment_intent
@@ -53,53 +51,59 @@ class SubscriptionCheckRequest(BaseModel):
 
 @app.post("/api/check_subscription/")
 async def check_payment_by(payload: SubscriptionCheckRequest):
-    data = {k: v for k, v in {
-        "email": payload.email,
-        "username": payload.username,
-        "user_id": payload.user_id
-    }.items() if v is not None}
-    logger.info(f"filters: {data}, types: {[type(v) for v in data.values()]}")
+    try:
+        data = {k: v for k, v in {
+            "email": payload.email,
+            "username": payload.username,
+            "user_id": payload.user_id
+        }.items() if v is not None}
+        logger.info(f"filters: {data}, types: {[type(v) for v in data.values()]}")
 
-    if not data:
-        return JSONResponse({"error": "Bad filters passed"}, status_code=400)
+        if not data:
+            return JSONResponse({"error": "Bad filters passed"}, status_code=400)
 
-    logger.info(f"[CHECK SUBSCRIPTION] New request with data {data.items()}...")
+        logger.info(f"[CHECK SUBSCRIPTION] New request with data {data.items()}...")
 
-    telegram_user = await get_telegram_user(**data)
-    if telegram_user:
-        logger.info(f"[CHECK SUBSCRIPTION] Found Telegram User! {telegram_user.user_id}")
-        return telegram_user
-
-    telegram_user = await create_telegram_user(**data)
-
-    logger.info(f"[CHECK SUBSCRIPTION] Telegram User not found, searching for customer...")
-    customers = await get_customers(email=data.get('email'), username=data.get('username'))
-
-    if not customers:
-        logger.info(f"[CHECK SUBSCRIPTION] No customer found {data.keys()} {data.values()}")
-        return JSONResponse({"message": "No customer found", "subscription_status": False}, status_code=200)
-
-    for cus in customers:
-        cus.user_id = telegram_user
-        await cus.save()
-
-        cid = cus.id
-        subscriptions = await get_subscriptions({"customer_id": cid})
-        active_sub = None
-        for sub in subscriptions:
-            if sub.status == "active":
-                telegram_user.subscription_status = True
-                telegram_user.date_end = sub.ending
-                await telegram_user.save()
-                active_sub = sub
-                break
-
-        if active_sub:
-            logger.info(f"[CHECK SUBSCRIPTION] Found subscription for {cid} {subscriptions}")
+        telegram_user = await get_telegram_user(**data)
+        if telegram_user:
+            logger.info(f"[CHECK SUBSCRIPTION] Found Telegram User! {telegram_user.user_id}")
             return telegram_user
 
-    logger.info(f"[CHECK SUBSCRIPTION] Subscription not found for {customers}")
-    return {"subscription_status": False, "message": "Not found subscription for customer"}
+        telegram_user = await create_telegram_user(**data)
+
+        logger.info(f"[CHECK SUBSCRIPTION] Telegram User not found, searching for customer...")
+        customers = await get_customers(email=data.get('email'), username=data.get('username'))
+
+        if not customers:
+            logger.info(f"[CHECK SUBSCRIPTION] No customer found {data.keys()} {data.values()}")
+            return JSONResponse({"message": "No customer found", "subscription_status": False}, status_code=200)
+
+        for cus in customers:
+            cus.user_id = telegram_user
+            await cus.save()
+
+            cid = cus.id
+            subscriptions = await get_subscriptions({"customer_id": cid})
+            active_sub = None
+            for sub in subscriptions:
+                if sub.status == "active":
+                    telegram_user.subscription_status = True
+                    telegram_user.date_end = sub.ending
+                    await telegram_user.save()
+                    active_sub = sub
+                    break
+
+            if active_sub:
+                logger.info(f"[CHECK SUBSCRIPTION] Found subscription for {cid} {subscriptions}")
+                return telegram_user
+
+        logger.info(f"[CHECK SUBSCRIPTION] Subscription not found for {customers}")
+        return {"subscription_status": False, "message": "Not found subscription for customer"}
+    except HTTPException:
+        raise  # пробрасываем HTTP ошибки как есть
+    except Exception as e:
+        logger.exception("[CHECK SUBSCRIPTION] Unexpected error")
+        return {"error": "Internal Server Error", "detail": str(e)}
 
 
 @app.post("/stripe_webhook")
