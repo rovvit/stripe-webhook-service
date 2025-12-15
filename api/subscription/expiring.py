8ab04ab5b26ade4bd40fc2d782573aa28c70cc4f
Rovvit <vitazzz96@gmail.com>
2025-12-15 15:51:23 +0200

    new method

--- api/subscription/expiring.py
+++ api/subscription/expiring.py
@@ -26,7 +26,7 @@ async def get_expiring_subscriptions(days: int = 5):
     ).all()
 
     logger.info(f"[GET EXPIRING SUBS] Found subscriptions: {users} and tg users:")
-    user_ids = [user["user_id"] for user in users]
+    user_ids = [user.user_id for user in users]
 
     return user_ids
 
