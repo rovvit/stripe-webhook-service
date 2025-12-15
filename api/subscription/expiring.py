67e30955efe9374b16dfa1d06a1f3ce053eea829
Rovvit <vitazzz96@gmail.com>
2025-12-15 15:54:25 +0200

    new method

--- api/subscription/expiring.py
+++ api/subscription/expiring.py
@@ -1,16 +1,7 @@
-from pydantic import BaseModel
-from typing import Optional
-
 from datetime import datetime, timedelta, UTC
 
-from db.models import Subscription
 from db.models import TelegramUser
 from utils.logger import logger
-from fastapi import HTTPException, APIRouter
-from starlette.responses import JSONResponse
-from db.Subscription import get_subscriptions
-from db.TelegramUser import get_telegram_user, create_telegram_user
-from db.Customer import get_customers
 from .router import router
 
 @router.get("/expiring")
@@ -22,11 +13,17 @@ async def get_expiring_subscriptions(days: int = 5):
     users = await TelegramUser.filter(
         date_end__gte=start,
         date_end__lt=end,
-        subscription_status=True
+        subscription_status=True,
+        is_admin=False
     ).all()
 
     logger.info(f"[GET EXPIRING SUBS] Found subscriptions: {users} and tg users:")
-    user_ids = [user.user_id for user in users]
+    user_ids = [
+        {
+            "user_id": user.user_id,
+            "date_end": user.date_end
+        }
+        for user in users]
 
     return user_ids
 
