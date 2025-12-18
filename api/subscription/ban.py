05f690ea711ad9ea28ebc75a7c662e7f300daa5d
Rovvit <vitazzz96@gmail.com>
2025-12-18 15:54:00 +0200

    New endpoint for bans

--- api/subscription/ban.py
+++ api/subscription/ban.py
@@ -1,10 +1,20 @@
+from pydantic import BaseModel
 from fastapi import Response, status
 from db.models import TelegramUser
 from utils.logger import logger
 from .router import router
 
+class BanUserRequest(BaseModel):
+    user_id: int
+
 @router.post("/ban")
-async def update_telegram_user_set_status_false(uid: int = None):
+async def update_telegram_user_set_status_false(request: BanUserRequest):
+    try:
+        uid = request.user_id
+    except Exception:
+        logger.error(f"[BAN] No user_id passed!")
+        return Response(status_code=status.HTTP_400_BAD_REQUEST)
+
     if not uid:
         logger.info(f"[BAN] No user id passed, skipped.")
         return Response(status_code=status.HTTP_204_NO_CONTENT)
