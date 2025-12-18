435f4072d8ce033dd01559fb47de7a7e97df0e23
Rovvit <vitazzz96@gmail.com>
2025-12-18 15:49:54 +0200

    New endpoint for bans

---
+++ api/subscription/ban.py
@@ -0,0 +1,24 @@
+from fastapi import Response, status
+from db.models import TelegramUser
+from utils.logger import logger
+from .router import router
+
+@router.post("/ban")
+async def update_telegram_user_set_status_false(uid: int = None):
+    if not uid:
+        logger.info(f"[BAN] No user id passed, skipped.")
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+    tgu = await TelegramUser.get_or_none(user_id=uid)
+    if not tgu:
+        logger.info(f"[BAN] User {uid} not found, skipped.")
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+    try:
+        tgu.subscription_status = False
+        await tgu.save()
+        logger.info(f"[BAN] User {uid} subscription status switch to False.")
+        return {'success': True}
+    except Exception as e:
+        logger.error(f"[BAN] Error occurred: {e}")
+        return Response(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
