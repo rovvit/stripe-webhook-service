61af5ed7a2e8793af1d86b6a7d48d6404ed053b2
Rovvit <vitazzz96@gmail.com>
2025-12-18 15:06:46 +0200

    Refactored to end_date query

--- api/subscription/expiring.py
+++ api/subscription/expiring.py
@@ -5,13 +5,15 @@ from utils.logger import logger
 from .router import router
 
 @router.get("/expiring")
-async def get_expiring_subscriptions(days: int = 5):
-    now = datetime.now(UTC)
-    start = now + timedelta(days=days)
-    end = start + timedelta(days=1)
+async def get_expiring_subscriptions(days: int = 5, end_date: datetime = datetime.now().date()):
+    if days <= 0:
+        days = 1
+
+    end = datetime(end_date.year, end_date.month, end_date.day).replace(hour=23, minute=59, second=59)
+    start_date = end_date - timedelta(days=days)
 
     users = await TelegramUser.filter(
-        date_end__gte=start,
+        date_end__gte=start_date,
         date_end__lt=end,
         subscription_status=True,
         is_admin=False
@@ -26,26 +28,3 @@ async def get_expiring_subscriptions(days: int = 5):
         for user in users]
 
     return user_ids
-
-@router.get('/expired')
-async def get_expired_subscriptions():
-    end = datetime.now(UTC)
-    start = end - timedelta(days=1)
-
-    users = await TelegramUser.filter(
-        date_end__gte=start,
-        date_end__lt=end,
-        subscription_status=True,
-        is_admin=False
-    ).all()
-
-    logger.info(f"[GET EXPIRED SUBS] Found subscriptions than expired today: {users}")
-    user_ids = [
-        {
-            "user_id": user.user_id,
-            "date_end": user.date_end
-        }
-        for user in users]
-
-    return user_ids
-
