a576c3a51fc7c2c1955310a572a62a35ffd0833c
Rovvit <vitazzz96@gmail.com>
2025-12-18 14:11:38 +0200

    GET expired today

--- api/subscription/expiring.py
+++ api/subscription/expiring.py
@@ -27,7 +27,25 @@ async def get_expiring_subscriptions(days: int = 5):
 
     return user_ids
 
+@router.get('/expired')
+async def get_expired_subscriptions():
+    end = datetime.now(UTC)
+    start = end - timedelta(days=1)
 
+    users = await TelegramUser.filter(
+        date_end__gte=start,
+        date_end__lt=end,
+        subscription_status=True,
+        is_admin=False
+    ).all()
 
+    logger.info(f"[GET EXPIRED SUBS] Found subscriptions than expired today: {users}")
+    user_ids = [
+        {
+            "user_id": user.user_id,
+            "date_end": user.date_end
+        }
+        for user in users]
 
+    return user_ids
 
