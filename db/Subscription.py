1246e0d9b1fcdad55614764331623fc17d184e56
Rovvit <vitazzz96@gmail.com>
2025-12-21 13:34:24 +0200

    removed check for date

--- db/Subscription.py
+++ db/Subscription.py
@@ -31,19 +31,19 @@ async def save_subscription(event):
             logger.info(f"[NEW] Created subscription {data.get('id')}")
             return
 
-        if data.get('created_at') > existing.updated:
-            await existing.update_from_dict(data).save()
-            logger.info(f"[UPDATE] Updated subscription {data.get('id')}")
+        # if data.get('created_at') > existing.updated:
+        #     await existing.update_from_dict(data).save()
+        #     logger.info(f"[UPDATE] Updated subscription {data.get('id')}")
+        # else:
+        partial_update = {
+            k: v for k, v in data.items()
+            if getattr(existing, k) in (None, "", [])
+        }
+        if partial_update:
+            await existing.update_from_dict(partial_update).save()
+            logger.info(f"[UPDATE] Partially updated subscription {data.get('id')}")
         else:
-            partial_update = {
-                k: v for k, v in data.items()
-                if getattr(existing, k) in (None, "", [])
-            }
-            if partial_update:
-                await existing.update_from_dict(partial_update).save()
-                logger.info(f"[UPDATE] Partially updated subscription {data.get('id')}")
-            else:
-                logger.info(f"[SKIP] No new data for subscription {data.get('id')}, skipped")
+            logger.info(f"[SKIP] No new data for subscription {data.get('id')}, skipped")
 
     except Exception as e:
         logger.error(f"[ERROR] [SUBSCRIPTION] {e}")
