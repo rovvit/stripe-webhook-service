5df7102fc0102a66ddeec6dd744a96299f035fff
Rovvit <vitazzz96@gmail.com>
2025-12-21 13:31:35 +0200

    fixed a bug with timezones

--- db/Subscription.py
+++ db/Subscription.py
@@ -12,7 +12,7 @@ async def save_subscription(event):
       "customer": customer,
       "started": datetime.fromtimestamp(body.get("current_period_start"), tz=timezone.utc),
       "ending": datetime.fromtimestamp(body.get("current_period_end"), tz=timezone.utc),
-      "created": datetime.fromtimestamp(event.get("created"), tz=timezone.utc),
+      "created_at": datetime.fromtimestamp(event.get("created"), tz=timezone.utc),
       "url": body.get("items").get("url"),
       "cancel_at_period_end": body.get("cancel_at_period_end")
     }
