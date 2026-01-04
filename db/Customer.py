c0a76d3553f7b0306311c0c4d9754fca1266287c
Rovvit <vitazzz96@gmail.com>
2026-01-04 15:12:00 +0200

    refactored Customer.py

--- db/Customer.py
+++ db/Customer.py
@@ -88,20 +88,13 @@ async def update_customer_username_from_checkout_session(event):
         return
 
     try:
-        logger.info(f"customer_id={customer_id}, custom_fields={custom_fields}, telegram_tag={telegram_tag}")
-        created_event = datetime.fromtimestamp(event.get("created"), tz=UTC)
-        existing = await Customer.get_or_none(id=customer_id)
-
-        if existing:
-            existing.telegram_tag = telegram_tag
-            existing.updated = created_event
-            await existing.save()
-        else:
-            await Customer.create(
-                id=customer_id,
-                telegram_tag=telegram_tag,
-                updated=created_event,
-            )
+        await Customer.update_or_create(
+            defaults={
+                "username": telegram_tag,
+                "updated": datetime.fromtimestamp(event.get("created"), tz=UTC),
+            },
+            id=customer_id,
+        )
         logger.info(f"[UPDATE USERNAME] Successfully updated {customer_id} with {telegram_tag}")
 
     except Exception as e:
