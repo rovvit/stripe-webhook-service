9ef13fceb4f39a1b2f23d952fa6712307ab1dbcd
Rovvit <vitazzz96@gmail.com>
2026-01-04 15:51:27 +0200

    subscription deletion handler

--- db/Subscription.py
+++ db/Subscription.py
@@ -70,7 +70,7 @@ async def delete_subscription(event):
     sub_id = body.get('id')
     status = body.get('status')
     ended_at = datetime.fromtimestamp(body.get('ended_at'), tz=UTC)
-    subscription = get_subscriptions({"id": sub_id})
+    subscription = await get_subscriptions({"id": sub_id})
 
     if not subscription:
         logger.info(f"[DELETE SUBSCRIPTION] No subscription {sub_id} found, skipped")
